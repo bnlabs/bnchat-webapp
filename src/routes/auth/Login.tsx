fce723c11c6cca7bdb57124a53a26eb06eeac054
Hy0tic <vothanhdat278@gmail.com>
2023-06-10 00:12:14 -0400

    connect login endpoint

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -3,6 +3,7 @@ import { FormEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../../redux/userSlice";
 import { RootState } from "../../redux/userStore";
+import axios from 'axios';
 
 function Login() {
 	const dispatch = useDispatch();
@@ -13,18 +14,46 @@ function Login() {
 		dispatch(actions.setUsername(event.currentTarget.username.value));
 	}
 
+	function handleLogin(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const payload = JSON.stringify({
+				"email": event.currentTarget.username.value,
+				"password": event.currentTarget.password.value
+			});
+
+			const config = {
+			method: 'post',
+			maxBodyLength: Infinity,
+			url: 'http://localhost:5077/auth/login',
+			headers: { 
+				'Content-Type': 'application/json'
+			},
+			data : payload
+			};
+
+			axios.request(config)
+				.then((response:any) => {
+					console.log(JSON.stringify(response.data));
+					dispatch(actions.setUsername(response?.data.username));
+				})
+				.catch((error:any) => {
+					console.log(error);
+				});
+	}
+
 	return (
-		<>
+		<div>
 			<form
 				action="submit"
 				className="flex justify-center"
-				onSubmit={handleSetUsername}
+				onSubmit={handleLogin}
 			>
 				<Input className="mr-2" placeholder="Username" name="username"></Input>
+				<Input className="mr-2" placeholder="Password" name="password"></Input>
 				<Button type="submit">Set username</Button>
 			</form>
 			<div>Username: {username}</div>
-		</>
+		</div>
 	);
 }
 
