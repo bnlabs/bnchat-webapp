336ccb2d153031998ca1f438c5ab22bd5647b987
Francisco Hauva <phauva@gmail.com>
2023-06-13 17:03:56 -0400

    Separate root route into it's own index component

--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { SignalRProvider } from "./components/SignalR/SignalRContext.tsx";
 import Login from "./routes/auth/Login.tsx";
 import Signup from "./routes/auth/Signup.tsx";
-import { Button, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import Chat from "./routes/Chat.tsx";
 import { Provider } from "react-redux";
 import userStore from "./redux/userStore.ts";
 import { Toaster } from "react-hot-toast";
-import axios from "axios";
-
-const apiUrl = import.meta.env.VITE_API_URL;
+import Index from "./routes/Index.tsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: (
-			<div className="flex justify-center h-screen items-center gap-2">
-				<Link to="/app">
-					<Button>Chat</Button>
-				</Link>
-				<Link to="/auth/login">
-					<Button>Login</Button>
-				</Link>
-				<Button
-					onClick={() => {
-						axios
-							.get(
-								`${apiUrl}/Message/getConversation?userId=2d0864cb-b289-4789-8bd6-8e451855a426`,
-								{
-									withCredentials: true,
-								}
-							)
-							.then((response) => {
-								console.log(response.data);
-							})
-							.catch((error) => {
-								console.log(error);
-							});
-					}}
-				>
-					Test endpoint
-				</Button>
-			</div>
-		),
+		element: <Index />,
 	},
 	{
 		path: "/app",
---
+++ src/routes/Index.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@mantine/core";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const Index = () => {
+	return (
+		<div className="flex justify-center h-screen items-center gap-2">
+			<Link to="/app">
+				<Button>Chat</Button>
+			</Link>
+			<Link to="/auth/login">
+				<Button>Login</Button>
+			</Link>
+			<Button
+				onClick={() => {
+					axios
+						.get(
+							`${apiUrl}/Message/getConversation?userId=2d0864cb-b289-4789-8bd6-8e451855a426`,
+							{
+								withCredentials: true,
+							}
+						)
+						.then((response) => {
+							console.log(response.data);
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				}}
+			>
+				Test endpoint
+			</Button>
+		</div>
+	);
+};
+
+export default Index;
