d01912fb90452e39d90ab96b021c0ef3902d8024
Francisco Hauva <phauva@gmail.com>
2023-06-13 04:58:09 -0400

    Add responsive sidebar.

--- src/routes/Chat.tsx
+++ src/routes/Chat.tsx
@@ -83,76 +83,79 @@ const Chat = () => {
 	}
 
 	return (
-		<div className="flex flex-1  flex-col items-center p-4 bg-slate-950 rounded-xl">
-			<div>
-				<label className="mb-2">Connection Status: {connectionStatus}</label>
+		<>
+			<div className="w-96 bg-slate-950 rounded-xl mr-2 max-lg:hidden"></div>
+			<div className="flex flex-1 flex-col items-center p-4 bg-slate-950 rounded-xl">
 				<div>
-					<form
-						className="flex items-center mb-2"
-						action="submit"
-						onSubmit={handleSetUsername}
-					>
-						<label className="mr-2">Username:</label>
-						<Input
-							className="mr-2"
-							name="username"
-							disabled={username !== "" || connectionStatus === "Closed"}
-							placeholder="Username"
-						></Input>
-						<Button
-							type="submit"
-							disabled={username !== "" || connectionStatus === "Closed"}
+					<label className="mb-2">Connection Status: {connectionStatus}</label>
+					<div>
+						<form
+							className="flex items-center mb-2"
+							action="submit"
+							onSubmit={handleSetUsername}
 						>
-							Set username
-						</Button>
-					</form>
-					<form
-						className="flex mb-2 items-center"
-						action="submit"
-						onSubmit={handleSetConversationId}
-					>
-						<label className="mr-2">Conversation:</label>
-						<Input
-							className="mr-2"
-							placeholder="Conversation"
-							name="conversationId"
-							disabled={username === "" || connectionStatus === "Closed"}
-						></Input>
-						<Button
-							type="submit"
-							disabled={username === "" || connectionStatus === "Closed"}
+							<label className="mr-2">Username:</label>
+							<Input
+								className="mr-2"
+								name="username"
+								disabled={username !== "" || connectionStatus === "Closed"}
+								placeholder="Username"
+							></Input>
+							<Button
+								type="submit"
+								disabled={username !== "" || connectionStatus === "Closed"}
+							>
+								Set username
+							</Button>
+						</form>
+						<form
+							className="flex mb-2 items-center"
+							action="submit"
+							onSubmit={handleSetConversationId}
 						>
-							Join conversation
-						</Button>
-					</form>
+							<label className="mr-2">Conversation:</label>
+							<Input
+								className="mr-2"
+								placeholder="Conversation"
+								name="conversationId"
+								disabled={username === "" || connectionStatus === "Closed"}
+							></Input>
+							<Button
+								type="submit"
+								disabled={username === "" || connectionStatus === "Closed"}
+							>
+								Join conversation
+							</Button>
+						</form>
+					</div>
+				</div>
+				<div className="flex flex-1 overflow-hidden w-full rounded bg-slate-800">
+					<ul
+						ref={messageWindow}
+						className="flex flex-col-reverse overflow-y-scroll no-scrollbar m-0 flex-1 p-2  list-none "
+					>
+						{messageHistory.map((messageData, index) => {
+							return (
+								<li className="my-1" key={index.toString()}>
+									<ChatMessage
+										username={messageData.senderName}
+										timestamp={messageData.timestamp}
+									>
+										{messageData.content}
+									</ChatMessage>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div className="flex w-full justify-center rounded mt-2 bg-slate-800 p-2">
+					<ChatSender
+						disabled={username === "" || connectionStatus === "Closed"}
+						handleSendMessage={handleSendMessage}
+					/>
 				</div>
 			</div>
-			<div className="flex flex-1 overflow-hidden w-full rounded bg-slate-800">
-				<ul
-					ref={messageWindow}
-					className="flex flex-col-reverse overflow-y-scroll no-scrollbar m-0 flex-1 p-2  list-none "
-				>
-					{messageHistory.map((messageData, index) => {
-						return (
-							<li className="my-1" key={index.toString()}>
-								<ChatMessage
-									username={messageData.senderName}
-									timestamp={messageData.timestamp}
-								>
-									{messageData.content}
-								</ChatMessage>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-			<div className="flex justify-center rounded mt-2 bg-slate-800 p-2">
-				<ChatSender
-					disabled={username === "" || connectionStatus === "Closed"}
-					handleSendMessage={handleSendMessage}
-				/>
-			</div>
-		</div>
+		</>
 	);
 };
 
