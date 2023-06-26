683ff576908d1e0aff2449136ab2246a32268cff
Hy0tic <vothanhdat278@gmail.com>
2023-06-26 01:31:04 -0400

    move pfp off to the side of the message

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -11,26 +11,29 @@ const ChatMessage = ({
 	children?: ReactNode;
 }) => {
 	return (
-		<div className="bg-slate-900 p-2 rounded-xl">
-			<div className="flex items-center">
-				<div className="flex flex-row items-center text-xs ">
-					<Avatar size={"xs"} />
-					<strong>{username}</strong>
-					<span className="text-xs text-gray-400 ml-2">
-						{(new Date(timestamp)).toLocaleString('en-UK', {
-							hour: "2-digit",
-							minute: "2-digit",
-							day: "2-digit",
-							month: "long",
-							year: "numeric",
-							hour12: true
-						})}
-					</span>
+		<div className="bg-slate-900 p-2 rounded-xl flex">
+			<Avatar size={"lg"} className="mr-2" />
+			<div>
+				<div className="flex items-center">
+					<div className="flex flex-row items-center text-xs ">
+						<strong>{username}</strong>
+						<span className="text-xs text-gray-400 ml-2">
+							{(new Date(timestamp)).toLocaleString('en-UK', {
+								hour: "2-digit",
+								minute: "2-digit",
+								day: "2-digit",
+								month: "long",
+								year: "numeric",
+								hour12: true
+							})}
+						</span>
+					</div>
 				</div>
-				
-				
+
+				<div className="break-words">
+					{children ?? ""}
+				</div>
 			</div>
-			<div className="break-words">{children ?? ""}</div>
 		</div>
 	);
 };
