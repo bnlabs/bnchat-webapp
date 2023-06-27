f5ade3e86aaaa798871b558a8b9fb015c9e4235a
Hy0tic <vothanhdat278@gmail.com>
2023-06-27 02:39:11 -0400

    move pfp to the side of messages inside conversation container, mute and deafen button are now togglable

--- src/components/Chat/ConversationContainer.tsx
+++ src/components/Chat/ConversationContainer.tsx
@@ -1,17 +1,21 @@
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
+import { Avatar } from '@mantine/core'
 
 
 const ConversationContainer = ({recipientName, latestMessage} : {recipientName:string,latestMessage:any }) => {
     TimeAgo.addDefaultLocale(en)
     const timeAgo = new TimeAgo('en-US');
-    return <div className="p-3">
-        <div className="p-1 font-extrabold">
-            {recipientName}
-        </div>
-        {latestMessage.senderName}: {latestMessage.content}
-        <div className='text-xs text-gray-500'>
-            {timeAgo.format(new Date(latestMessage.timestamp))}
+    return <div className="p-3 flex">
+        <Avatar size={"lg"} className="mr-3" />
+        <div>
+            <div className="p-1 font-extrabold">
+                {recipientName}
+            </div>
+            {latestMessage.senderName}: {latestMessage.content}
+            <div className='text-xs text-gray-500'>
+                {timeAgo.format(new Date(latestMessage.timestamp))}
+            </div>
         </div>
     </div>
 }
